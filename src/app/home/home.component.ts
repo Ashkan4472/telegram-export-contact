3ddd989af238f89e9351e933aab2ce83b1e9b4ef
Ashkan4472 <Ashkan4472@gmail.com>
2020-02-05 22:31:51 +0330

    added logic for vcf export

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../core/services/notification.service';
+import { ContactModel } from '../models/contact.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { NotificationService } from '../core/services/notification.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  textVcf = '';
 
   constructor(
     private readonly notificationService: NotificationService,
@@ -16,11 +19,33 @@ export class HomeComponent implements OnInit {
   }
 
   fileUpload(files: File[]) {
-    // TODO: Need to handle file in next page
     if (files && files.length > 0) {
       const fileType = files[0].type;
       if (fileType.indexOf('json') === -1) {
         this.notificationService.primary('file should be in json format.');
+      } else {
+        const reader = new FileReader();
+        reader.onloadend = (e) => {
+          const tmp = JSON.parse(reader.result as string);
+          if (tmp && tmp.contacts && tmp.contacts.list) {
+            for (const c of tmp.contacts.list) {
+              if (c.phone_number) {
+                this.textVcf += ContactModel.getVcfText(c.first_name, c.last_name, c.phone_number);
+              }
+            }
+            const blob = new Blob([this.textVcf], {
+              type: 'text/vcard',
+            });
+            const blobUrl = window.URL.createObjectURL(blob);
+            const anchor = document.createElement('a');
+            anchor.download = 'output.vcf';
+            anchor.href = blobUrl;
+            anchor.click();
+          } else {
+            this.notificationService.danger('your file does not have contact in it!');
+          }
+        };
+        reader.readAsText(files[0]);
       }
     }
   }
---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,10 @@
+export class ContactModel {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+
+  static getVcfText(firstName: string, lastName: string, phoneNumber: string) {
+    // tslint:disable-next-line: max-line-length
+    return `BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:${firstName ? firstName + ' ' : ''}${lastName}\nN;CHARSET=UTF-8:${firstName};${lastName};;;\nTEL;TYPE=CELL:${phoneNumber}\nREV:${(new Date()).toISOString()}\nEND:VCARD\n`;
+  }
+}
