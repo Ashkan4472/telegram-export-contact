07a47b76482c7993e64a8fae2bc992b28cece5db
Sina Poyanfar <sinapoyanfar1379@gmail.com>
2020-03-08 23:26:35 +0330

    added data show

--- src/app/selection/ contact-item/contact-item.component.ts
+++ src/app/selection/ contact-item/contact-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-contact-item' ,
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-item.component.scss'],
 })
 export class ContactItemComponent {
-  constructor() {}
+  @Input() firstName: string;
+  @Input() lastName: string;
+  @Input() phoneNumber: string;
+
+  constructor(
+    private readonly notificationService: NotificationService,
+  ) {}
+
+  deleteContact() {
+    this.notificationService.warning('this feature will available later');
+  }
 }
--- src/app/selection/selection.module.ts
+++ src/app/selection/selection.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { SelectionComponent } from './selection.component';
 import { ContactItemComponent } from './ contact-item/contact-item.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+  ],
   declarations: [
     SelectionComponent,
     ContactItemComponent,
